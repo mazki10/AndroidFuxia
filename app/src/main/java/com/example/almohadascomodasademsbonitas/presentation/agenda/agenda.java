f1cb668953c151ff164eef09cca6ae7ba64f9cf8
mazki10 <mazkigazte@gmail.com>
2023-11-27 16:55:04 +0100

    Generando estructura de carpetas

---
+++ app/src/main/java/com/example/almohadascomodasademsbonitas/presentation/agenda/agenda.java
@@ -0,0 +1,16 @@
+package com.example.almohadascomodasademsbonitas.presentation.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.almohadascomodasademsbonitas.R;
+
+public class agenda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agenda);
+    }
+}
