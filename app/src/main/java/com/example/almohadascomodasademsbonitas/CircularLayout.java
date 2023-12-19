0fe3e4f3470ad39df205a47a3e31aeb4752da2b1
mazki10 <mazkigazte@gmail.com>
2023-12-19 05:32:40 +0100

    se ha añadido el nuevo menu

--- app/src/main/java/com/example/almohadascomodasademsbonitas/CircularLayout.java
+++ app/src/main/java/com/example/almohadascomodasademsbonitas/CircularLayout.java
@@ -2,19 +2,24 @@ package com.example.almohadascomodasademsbonitas;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class CircularLayout extends View {
 
-    private static final int NUM_ITEMS = 6;
+    private static final int NUM_ITEMS = 5;
     private Paint paint = new Paint();
     private float[] cx = new float[NUM_ITEMS];
     private float[] cy = new float[NUM_ITEMS];
+    private float[] currentCx = new float[NUM_ITEMS];
+    private float[] currentCy = new float[NUM_ITEMS];
     private float radius;
     private float centerX;
     private float centerY;
@@ -34,8 +39,7 @@ public class CircularLayout extends View {
     }
 
     private void init() {
-        // Inicializa el Paint para los círculos
-        paint.setColor(Color.BLACK);
+        paint.setColor(Color.WHITE);
         paint.setStyle(Paint.Style.FILL);
 
         // Configura eventos táctiles
@@ -76,8 +80,12 @@ public class CircularLayout extends View {
     private void rotateCircles(float angle) {
         for (int i = 0; i < NUM_ITEMS; i++) {
             double newAngle = angle + (i * (2 * Math.PI) / NUM_ITEMS);
-            cx[i] = centerX + radius * (float) Math.cos(newAngle);
-            cy[i] = centerY + radius * (float) Math.sin(newAngle);
+            currentCx[i] = centerX + radius * (float) Math.cos(newAngle);
+            currentCy[i] = centerY + radius * (float) Math.sin(newAngle);
+
+            // Actualiza las posiciones de las imágenes para su dibujo
+            cx[i] = currentCx[i];
+            cy[i] = currentCy[i];
         }
     }
 
@@ -102,9 +110,31 @@ public class CircularLayout extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // Dibuja los círculos
+        Drawable[] images = new Drawable[NUM_ITEMS];
+        images[0] = getResources().getDrawable(R.drawable.agenda);
+        images[1] = getResources().getDrawable(R.drawable.enviar);
+        images[2] = getResources().getDrawable(R.drawable.pedidos);
+        images[3] = getResources().getDrawable(R.drawable.informacion);
+        images[4] = getResources().getDrawable(R.drawable.partners);
+
         for (int i = 0; i < NUM_ITEMS; i++) {
-            canvas.drawCircle(cx[i], cy[i], 20, paint);
+            if (images[i] != null) {
+                Bitmap bitmap = ((BitmapDrawable) images[i]).getBitmap();
+                int width = bitmap.getWidth();
+                int height = bitmap.getHeight();
+
+                // Escalar la imagen al tamaño deseado (puedes ajustar estos valores)
+                int scaledWidth = 250; // Ancho escalado deseado
+                int scaledHeight = 250; // Altura escalada deseada
+
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, scaledWidth, scaledHeight, true);
+                images[i] = new BitmapDrawable(getResources(), scaledBitmap);
+
+                // Dibujar la imagen escalada en el lienzo
+                images[i].setBounds((int) (cx[i] - scaledWidth / 2), (int) (cy[i] - scaledHeight / 2),
+                        (int) (cx[i] + scaledWidth / 2), (int) (cy[i] + scaledHeight / 2));
+                images[i].draw(canvas);
+            }
         }
     }
 }
