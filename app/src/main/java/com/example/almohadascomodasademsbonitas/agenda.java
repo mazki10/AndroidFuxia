9a50f03dbbe54df09abfef99703f0d910bb0d533
mazki10 <mazkigazte@gmail.com>
2024-01-06 00:53:26 +0100

    actividad agenda

--- app/src/main/java/com/example/almohadascomodasademsbonitas/agenda.java
+++ app/src/main/java/com/example/almohadascomodasademsbonitas/agenda.java
@@ -1,19 +1,33 @@
 package com.example.almohadascomodasademsbonitas;
 
+import static java.security.AccessController.getContext;
+
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Xml;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import org.xmlpull.v1.XmlPullParser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class agenda extends AppCompatActivity {
     private TextView tv;
-    private Button bt, btSig, btAnt;
+    private Button btBus, btSig, btAnt, btAn;
+    private ListView lvAct;
     private String[] nombreMeses;
 
     private Calendar cal = Calendar.getInstance();
@@ -21,77 +35,136 @@ public class agenda extends AppCompatActivity {
     private int mes = cal.get(Calendar.MONTH);
     private int dia = cal.get(Calendar.DAY_OF_MONTH);
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
 
         tv = findViewById(R.id.tv);
-        bt = findViewById(R.id.bt);
+        btBus = findViewById(R.id.btBus);
         btSig = findViewById(R.id.btSigMes);
         btAnt = findViewById(R.id.btAntMes);
+        btAn = findViewById(R.id.btNue);
+        lvAct = findViewById(R.id.lvAg);
 
-        nombreMeses = getResources().getStringArray(R.array.nombremeses); // Array con los nombres de los meses
-
-        // Obtener la fecha actual
-
+        nombreMeses = getResources().getStringArray(R.array.nombremeses);
 
         String nombreMesActual = nombreMeses[mes];
         String fecha = nombreMesActual + " " + anio;
         tv.setText(fecha);
 
-        bt.setOnClickListener(new View.OnClickListener() {
+        // Obtén la lista de datos desde el XML
+        ArrayList<String> datosDeXml = leerDatosDesdeXml();
+
+        // Crea un ArrayAdapter para enlazar los datos al ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, datosDeXml);
+
+        // Asigna el adaptador al ListView
+        lvAct.setAdapter(adapter);
+
+        btBus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 abrirCalendario(v);
             }
         });
-
-        btSig.setOnClickListener(new View.OnClickListener() {
+        btAn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mes < 11) {
-                    mes++;
-                    String nombreMesActual = nombreMeses[mes];
-                    String fecha = nombreMesActual + " " + anio;
-                    tv.setText(fecha);
-                }
-
-                if (mes >= 11) {
-                    btSig.setEnabled(false); // Desactiva el botón si mes es mayor o igual a 11
-                }
+                nuevoAgenda(v);
             }
         });
-
-
         btAnt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mes > 0) {
                     mes--;
-                    String nombreMesActual = nombreMeses[mes - 1];
-                    String fecha = nombreMesActual + " " + anio;
-                    tv.setText(fecha);
+                } else {
+                    mes = 11;
+                    anio--;
                 }
 
-                if (mes <= 0) {
-                    btAnt.setEnabled(false); // Desactiva el botón si mes es menor o igual a 0
-                }
+                String nombreMesActual = nombreMeses[mes];
+                String fecha = nombreMesActual + " " + anio;
+                tv.setText(fecha);
             }
         });
+        btSig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mes < 11) {
+                    mes++;
+                } else {
+                    mes = 0;
+                    anio++;
+                }
 
+                String nombreMesActual = nombreMeses[mes];
+                String fecha = nombreMesActual + " " + anio;
+                tv.setText(fecha);
+            }
+        });
     }
-
     public void abrirCalendario(View v) {
-
         DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                String fecha = dayOfMonth + "/" + month + "/" + year;
+                anio = year;
+                mes = month;
+
+                String nombreMesActual = nombreMeses[month];
+                String fecha = nombreMesActual + " " + year;
                 tv.setText(fecha);
             }
         }, anio, mes, dia);
         dpd.show();
+    }
+
+    private ArrayList<String> leerDatosDesdeXml() {
+        ArrayList<String> datosDeXml = new ArrayList<>();
+
+        try {
+            File directory = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+            File file = new File(directory, "actividades.xml");
+            FileInputStream fis = new FileInputStream(file);
+
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(fis, null);
+
+            int eventType = parser.getEventType();
+            StringBuilder currentData = new StringBuilder();
+            boolean inActividad = false;
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    if ("actividad".equals(parser.getName())) {
+                        currentData = new StringBuilder();
+                        inActividad = true;
+                    }
+                } else if (eventType == XmlPullParser.TEXT) {
+                    currentData.append(parser.getText());
+                } else if (eventType == XmlPullParser.END_TAG) {
+                    if ("actividad".equals(parser.getName())) {
+                        datosDeXml.add(currentData.toString().trim());
+                        inActividad = false;
+                    }
+                }
+
+                eventType = parser.next();
+            }
+
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return datosDeXml;
+    }
+
+    public void nuevoAgenda(View v) {
+        Intent intent = new Intent(this, nuevo_agenda.class);
+
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/almohadascomodasademsbonitas/nuevo_agenda.java
@@ -0,0 +1,130 @@
+package com.example.almohadascomodasademsbonitas;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Xml;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class nuevo_agenda extends AppCompatActivity {
+    private EditText edtTit, edtDes;
+    private Button btFec, btNue;
+    private String FILE_NAME = "actividades.xml";
+
+    private Calendar cal = Calendar.getInstance();
+    private int anio = cal.get(Calendar.YEAR);
+    private int mes = cal.get(Calendar.MONTH);
+    private int dia = cal.get(Calendar.DAY_OF_MONTH);
+    private int hora = cal.get(Calendar.HOUR_OF_DAY);
+    private int minuto = cal.get(Calendar.MINUTE);
+    private String fechaSeleccionada;
+    private String horaSeleccionada;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_agenda);
+
+        edtTit = findViewById(R.id.edtTit);
+        edtDes = findViewById(R.id.edtDes);
+        btFec = findViewById(R.id.botFec);
+        btNue = findViewById(R.id.botNue);
+
+        btFec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                devolverHora(v);
+                devolverFecha(v);
+            }
+        });
+
+        btNue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                agregarActividad();
+                finish();
+            }
+        });
+    }
+    private void agregarActividad() {
+        String titulo = edtTit.getText().toString();
+        String descripcion = edtDes.getText().toString();
+        String hora = obtenerHoraSeleccionada();
+        String fecha = obtenerFechaSeleccionada();
+        try {
+            File directory = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+            File file = new File(directory, FILE_NAME);
+            FileOutputStream fileos = new FileOutputStream(file, true); // 'true' para añadir al archivo existente
+
+            XmlSerializer xmlSerializer = Xml.newSerializer();
+            xmlSerializer.setOutput(fileos, "UTF-8");
+
+            xmlSerializer.startTag(null, "actividad");
+
+            xmlSerializer.startTag(null, "titulo");
+            xmlSerializer.text(titulo);
+            xmlSerializer.endTag(null, "titulo");
+
+            xmlSerializer.startTag(null, "descripcion");
+            xmlSerializer.text(descripcion);
+            xmlSerializer.endTag(null, "descripcion");
+
+            xmlSerializer.startTag(null, "fecha");
+            xmlSerializer.text(fecha);
+            xmlSerializer.endTag(null, "fecha");
+
+            xmlSerializer.startTag(null, "hora");
+            xmlSerializer.text(hora);
+            xmlSerializer.endTag(null, "hora");
+
+            xmlSerializer.endTag(null, "actividad");
+            xmlSerializer.endDocument();
+
+            xmlSerializer.flush();
+            fileos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String obtenerFechaSeleccionada() {
+        return fechaSeleccionada;
+    }
+
+    public String obtenerHoraSeleccionada() {
+        return horaSeleccionada;
+    }
+    public void devolverFecha(View v) {
+        DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                fechaSeleccionada = dayOfMonth + "/" + (month + 1) + "/" + year;
+            }
+        }, anio, mes, dia);
+        dpd.show();
+    }
+    public void devolverHora(View v) {
+        TimePickerDialog tpd = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                horaSeleccionada = String.format(Locale.getDefault(), "%02d:%02d", hourOfDay, minute);
+            }
+        }, hora, minuto, true);
+        tpd.show();
+    }
+}
