26b5a27e91ff10d13c1a2fc6828c076cd62ad4ed
ikere <ikereraun@gmail.com>
2024-01-10 08:08:51 +0100

    iker logra hacer pedidos despues de 100 años

--- app/src/main/java/com/example/almohadascomodasademsbonitas/pedidos/pagina2pedido.java
+++ app/src/main/java/com/example/almohadascomodasademsbonitas/pedidos/pagina2pedido.java
@@ -1,9 +1,6 @@
 package com.example.almohadascomodasademsbonitas.pedidos;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Xml;
